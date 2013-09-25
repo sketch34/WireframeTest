a89723ff7fd4de5260b027b9362f4593f90a6954
Jarrod <jarrod.smith@iinet.net.au>
2013-09-25 12:36:35 +1000

    Added IndexedPlane.

---
+++ src/IndexedPlane.cpp
@@ -0,0 +1,9 @@
+//
+//  IndexHelper.cpp
+//  WireframeTest
+//
+//  Created by Jarrod Smith on 25/09/13.
+//
+//
+
+#include "IndexHelper.h"
---
+++ src/IndexedPlane.h
@@ -0,0 +1,14 @@
+//
+//  IndexHelper.h
+//  WireframeTest
+//
+//  Created by Jarrod Smith on 25/09/13.
+//
+//
+
+#ifndef __WireframeTest__IndexHelper__
+#define __WireframeTest__IndexHelper__
+
+#include <iostream>
+
+#endif /* defined(__WireframeTest__IndexHelper__) */
